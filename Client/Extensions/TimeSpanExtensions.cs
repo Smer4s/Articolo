e10015d434ab9e879bbd474460bd87e83b641626
n.studnikita <studnikita.work@gmail.com>
2024-04-12 16:03:51 +0300

    Completed date parsing

--- Client/Extensions/TimeSpanExtensions.cs
+++ Client/Extensions/TimeSpanExtensions.cs
@@ -4,23 +4,58 @@ namespace Client.Extensions;
 
 public static class TimeSpanExtensions
 {
-    public static string ToTimeString(this TimeSpan value)
+    public static string ToTimeIntervalString(this DateTime created)
     {
-        var minutes = (int)Math.Round(value.TotalMinutes);
+        var minutes = (int)Math.Round((DateTime.Now - created).TotalMinutes);
 
-        if (minutes < 0) return "Только что";
+        if (minutes <= 5) return "Только что";
 
-        if (minutes > 60)
+        if (minutes >= 60)
         {
-            if (minutes > 60 * 24) return ToDateString(minutes);
+            if (minutes >= 60 * 24) return ToDateString(created);
+
             return ToHoursString(minutes / 60);
         }
+
         else return ToMinutesString(minutes);
     }
 
+    private static string ToDateString(DateTime created)
+    {
+        var sb = new StringBuilder();
+
+        sb.Append(created.Day.ToString() + " ");
+        sb.Append(created.Month.ToRussianMonthString());
+        if (DateTime.Now.Year > created.Year) sb.Append(created.Year);
+
+        return sb.ToString();
+    }
+
+    private static string ToRussianMonthString(this int month) =>
+        month switch
+        {
+            1 => "января ",
+            2 => "февраля ",
+            3 => "марта ",
+            4 => "апреля ",
+            5 => "мая ",
+            6 => "июня ",
+            7 => "июля ",
+            8 => "августа ",
+            9 => "сентября ",
+            10 => "октября ",
+            11 => "ноября ",
+            12 => "декабря ",
+            _ => throw new ArgumentOutOfRangeException(nameof(month))
+        };
+
     private static string ToMinutesString(int minutes)
     {
-        return $"{minutes} мин назад";
+        if (minutes >= 5 && minutes <= 20) return $"{minutes} минут назад";
+        if (minutes % 10 == 1) return $"{minutes} минуту назад";
+        if (minutes % 10 >= 2 && minutes % 10 <= 4) return $"{minutes} минуты назад";
+
+        throw new ArgumentOutOfRangeException(nameof(minutes));
     }
 
     private static string ToHoursString(int hours)
@@ -28,10 +63,5 @@ public static class TimeSpanExtensions
         if (hours >= 5 && hours <= 20) return $"{hours} часов назад";
         if (hours % 10 >= 2 && hours % 10 <= 4) return $"{hours} часа назад";
         return $"{hours} час назад";
-    }
-
-    private static string ToDateString(int minutes)
-    {
-        return DateTime.FromBinary(minutes).ToShortDateString();
     }
 }
